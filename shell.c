6b722bbd4b15c52a82fc04f62e6aabb58b4d460e
okidoki-jpg <okuhlen.20@gmail.com>
2022-11-07 16:53:50 +0200

    reordered command initialization

--- shell.c
+++ shell.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+
+char *strconcat(char *argv1, char *argv2)
+{
+	int i = 0, j = 0;
+	char *result = malloc(sizeof(argv1) * 7);
+
+	while (argv1[i])
+	{
+		result[j] = argv1[i];
+		i++;
+		j++;
+	}
+
+	i = 0; 
+
+	while (argv2[i])
+	{
+		result[j] = argv2[i];
+		i++;
+		j++;
+	}
+
+	result[j] = '\0';
+
+	return (result);
+}
 
 int main(void)
 {
@@ -15,34 +43,42 @@ int main(void)
 		printf(": ");
 		getline(&buff, &size, stdin);
 
-		token = strtok(&buff, ' ');
+
+		token = strtok(buff, " ");
 		while (*token)
 		{
+			if (i == 0)
+			{
+				if (access(token, F_OK))
+				{
+					command = strconcat("/bin/", token);
+					if (access(command, F_OK))
+					{
+						perror("Error");
+						printf("%s, Error\n", command);
+						/* clear memory & restart */
+						break;
+					}
+
+					else
+						command = token;
+				}
+			}
+
+
 			av[i] = malloc(strlen(token));
 			while (*token)
 				av[i][j++] = *token++;
 			av[i][j] = '\0';
 			i++;
-			token = strtok(NULL, ' ');
+			j = 0;
+			token = strtok(NULL, " ");
 		}
 
-		if (access(av[1], F_OK))
-		{
-			command = strconcat("/bin/", av[1]);
-			if (access(command, F_OK))
-			{
-				perror("Error");
-				/* clear memory & restart */
-				continue;
-			}
 
-			else
-				command = av[1];
-		}
-
-		if (execve(command, av++, NULL) == -1)
+		if (execve(command, av, NULL) == -1)
 		{
-			perror("Error");
+			perror("exec, Error");
 			/* clear memory & restart */
 			continue;
 		}
